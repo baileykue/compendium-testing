a126d6d3403c69abd215f989f5048a214f0adef9
Bailey Kuebelbeck <bailey.kue@gmail.com>
2022-01-24 17:49:03 -0800

    wrote  a fetch function for bobs burgers api, set up basic file structre

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { getCharacters } from './services/bobs-burgers';
+import { useEffect, useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getCharacters();
+      setCharacters(data);
+    };
+    fetchData();
+  }, []);
+  console.log(characters);
+  return <div className="App"></div>;
 }
 
 export default App;
