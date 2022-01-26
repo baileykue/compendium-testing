ae5ec8e3bd02b61bcfe93109be8fa0a7f93fb078
Bailey Kuebelbeck <bailey.kue@gmail.com>
2022-01-26 12:16:06 -0800

    done all the styling for this project, couldnt get public folder image to render *shrug*

--- src/components/CharacterList/CharacterList.jsx
+++ src/components/CharacterList/CharacterList.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 
 export default function CharacterList({ characters }) {
   return (
-    <div>
+    <div className="page">
       <ul className="list">
         {characters.map((character) => (
           <li key={character.id} className="card">
-            <img src={character.image} className="selfie" />
+            {character.image ? (
+              <img src={character.image} className="selfie" />
+            ) : (
+              <img scr={`${process.env.PUBLIC_URL}/profile/profile.png`} className="selfie" />
+            )}
             <h4>Name: {character.name}</h4>
             {character.dateOfBirth && <p>day of birth: {character.dateOfBirth}</p>}
             {character.patronus && <p>patronus: {character.patronus}</p>}
